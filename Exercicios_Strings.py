bc878fe9658514ad47781b99ae2c831be3d1bfa5
Lais-Ribeiro <74383107+Lais-Ribeiro@users.noreply.github.com>
2022-12-27 11:30:05 -0300

    criado o arquivo de exercícios de strings
    Neste arquivo foi feito exercícios básicos para exercitar o uso das Strings.

---
+++ Exercicios_Strings.py
@@ -0,0 +1,40 @@
+# 1. Crie um programa para cadastro de CPF de clientes que recebe o CPF em um input.Caso o usuário digite menos de 11 caracteres (tamanho do CPF brasileiro), o programa deve exibir uma mensagem de "Digite seu CPF corretamente"
+
+cpf = input('Informe o CPF: ')
+
+cpf_tratado = cpf.strip().replace('.', '').replace('-', '')
+
+if len(cpf_tratado) == 11 and cpf_tratado.isnumeric():
+    print('Cadastro efetuado. O CPF {} foi cadastrado com sucesso!'.format(cpf_tratado))
+else:
+    print('Digite seu CPF corretamente!')
+
+#2. Crie um programa que permita o cadastro de nome e e-mail de uma pessoa (por meio de inputs) e que verifique se nome e e-mail foram preenchidos, caso contrário ele deve avisar para preencher todos os dados corretamente e se o e-mail contém '@' e se depois do '@' existe algum '.', caso contrário ele deve exibir uma mensagem de e-mail inválido
+nome = input('Digite seu nome: ')
+email = input('Digite seu e-mail: ')
+
+if nome and email:
+    if '@' in email and '.' in email[email.find('@'):]:
+        print('Cadastro efetuado com sucesso!')
+    else:
+        print('Digite o e-mail corretamente')
+else:
+    print('Preencha todos os dados corretamente!')
+
+#3.Tamanho de strings. Faça um programa que leia 2 strings e informe o conteúdo delas seguido do seu comprimento. Informe também se as duas strings possuem o mesmo comprimento e são iguais ou diferentes no conteúdo.
+
+txt_1 = input('Digite o primeiro texto: ')
+txt_2 = input('Digite o segundo texto: ')
+
+print('A primeira String é "{}" e possui {} caracteres.'.format(txt_1, len(txt_1)))
+print('A segunda String é "{}" e possui {} caracteres.'.format(txt_2, len(txt_2)))
+
+if len(txt_1) == len(txt_2):
+    print('As duas Strings possui o mesmo tamanho!')
+else:
+    print('As duas Strings possuem tamanhos diferentes!')
+    
+if txt_1 == txt_2:
+    print('As duas Strings são iguais!')
+else:
+    print('As duas Strings são diferentes!')

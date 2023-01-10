620bd41398cab9ea38af6d2995357acef2c0e9d9
Lais-Ribeiro <74383107+Lais-Ribeiro@users.noreply.github.com>
2023-01-10 11:18:50 -0300

    Update Exercicios_While.py

--- Exercicios_While.py
+++ Exercicios_While.py
@@ -40,38 +40,8 @@ while True:
         print("Cadastro realizado com sucesso!")
         break
 
-#terminar
-#4. Faça um programa que leia e valide as seguintes informações (e para cada uma delas, continue pedindo a informação até o usuário inserir corretamente):
 
-#Nome: maior que 3 caracteres;
-#Idade: entre 0 e 150;
-# Salário: maior que zero;
-# Sexo: 'f' ou 'm';
-# Estado Civil: 's', 'c', 'v', 'd';
-
-nome = input("Informe seu nome: ")
-idade = int(input("Informe sua idade: "))
-# salario = int(input("Infome seu salario: R$ "))
-# sexo = input("Informe seu sexo: ")
-# estado_civil = input("Informe seu Estado Civil: ")
-
-while True:
-    nome = input("Informe seu nome: ")
-    if len(nome) < 3:
-        print("Nome inválido, digite novamente!")
-        nome = input("Informe seu nome: ")
-    if not (idade >= 0 and idade <= 150):
-        print("Idade inválida, digite novamente!")
-        idade = input("Informe sua idade: ")
-    else:
-        print("Cadastro realizado com sucesso!")
-        break
-
-#5. Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
-
-#6. Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. Valide a entrada e permita repetir a operação.
-
-#7. Faça um programa que peça para o usuário inserir o faturamento dos últimos 5 meses (individualmente) e informe o maior faturamento#7. Faça um programa que peça para o usuário inserir o faturamento dos últimos 5 meses (individualmente) e informe o maior faturamento
+#4. Faça um programa que peça para o usuário inserir o faturamento dos últimos 5 meses (individualmente) e informe o maior faturamento#7. Faça um programa que peça para o usuário inserir o faturamento dos últimos 5 meses (individualmente) e informe o maior faturamento
 i =  1
 faturamento = 0
 while i <= 5:
@@ -82,7 +52,7 @@ while i <= 5:
 
 print("O maior faturamento foi de R$ {:.2f}.".format(faturamento))
 
-#8. Faça um programa que peça para o usuário inserir o faturamento dos últimos 5 meses (individualmente) e informe o faturamento total (soma) e o faturamento médio por mês (média).
+#5. Faça um programa que peça para o usuário inserir o faturamento dos últimos 5 meses (individualmente) e informe o faturamento total (soma) e o faturamento médio por mês (média).
 
 i =  1
 faturamento = 0
@@ -95,9 +65,7 @@ media = faturamento / 5
 
 print("O faturamento total foi de R$ {:.2f} e o faturamento médio foi de R$ {:.2f}." .format(faturamento, media))
 
-#9.  Faça um programa que consiga categorizar a idade das equipes de uma empresa. Faça um programa que peça para n pessoas a sua idade, ao final o programa devera verificar se a média de idade da equipe varia entre 0 e 25 (jovem) ,26 e 60 (sênior) e maior que 60 (idosa); e então, dizer se a equipe é jovem, sênior ou idosa, conforme a média calculada.
-
-#10. Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores. Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
+#6. Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores. Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
 
 eleitores = int(input("Informe o número de eleitores: "))
 
@@ -125,32 +93,3 @@ print("O candidato 1 recebeu {} voto(s).".format(candidato1))
 print("O candidato 2 recebeu {} voto(s).".format(candidato2))
 print("O candidato 2 recebeu {} voto(s).".format(candidato3))
 
-
-#11. Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores. Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
-
-#12. O Sr. Manoel Joaquim possui uma grande loja de artigos de R$ 1,99, com cerca de 10 caixas. Para agilizar o cálculo de quanto cada cliente deve pagar ele desenvolveu um tabela que contém o número de itens que o cliente comprou e ao lado o valor da conta. Desta forma a atendente do caixa precisa apenas contar quantos itens o cliente está levando e olhar na tabela de preços. Você foi contratado para desenvolver o programa que monta esta tabela de preços, que conterá os preços de 1 até 50 produtos, conforme o exemplo abaixo:
-
-##12. O Sr. Manoel Joaquim possui uma grande loja de artigos de R\\$ 1,99, com cerca de 10 caixas. Para agilizar o cálculo de quanto cada cliente deve pagar ele desenvolveu um tabela que contém o número de itens que o cliente comprou e ao lado o valor da conta. Desta forma a atendente do caixa precisa apenas contar quantos itens o cliente está levando e olhar na tabela de preços. Você foi contratado para desenvolver o programa que monta esta tabela de preços, que conterá os preços de 1 até 50 produtos, conforme o exemplo abaixo:
-
-# Lojas Quase Dois - Tabela de preços
-# 1 - R$ 1.99
-# 2 - R$ 3.98
-# ...
-# 50 - R$ 99.50
-
-#13. Um funcionário de uma empresa recebe aumento salarial anualmente: Sabe-se que:
-
-# Esse funcionário foi contratado em 1995, com salário inicial de R$ 1.000,00;
-# Em 1996 recebeu aumento de 1,5% sobre seu salário inicial;
-# A partir de 1997 (inclusive), os aumentos salariais sempre correspondem ao dobro do percentual do ano anterior. Faça um programa que determine o salário desse funcionário em 2003. 
-
-#14. O cardápio de uma lanchonete é o seguinte:
-
-# Especificação   Código  Preço
-# Cachorro Quente 100     R$ 1,20
-# Bauru Simples   101     R$ 1,30
-# Bauru com ovo   102     R$ 1,50
-# Hambúrguer      103     R$ 1,20
-# Cheeseburguer   104     R$ 1,30
-# Refrigerante    105     R$ 1,00
-# Faça um programa que leia o código dos itens pedidos e as quantidades desejadas. Calcule e mostre o valor a ser pago por item (preço * quantidade) e o total geral do pedido. Considere que o cliente deve informar quando o pedido deve ser encerrado.

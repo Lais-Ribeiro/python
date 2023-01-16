2acf7b4fc6156650ab2fa6058116249ddeee7f8f
Lais-Ribeiro <74383107+Lais-Ribeiro@users.noreply.github.com>
2023-01-16 11:18:32 -0300

    Create Exercicios_Mentoria.py
    Exercicios de fixação realizado na mentoria do curso da Hasthag

---
+++ Exercicios_Mentoria.py
@@ -0,0 +1,8 @@
+''' Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R\$ 80,00 ou em galões de 3,6 litros, que custam R\$ 25,00.'''
+
+#pedir o tamanho em metros quadrados
+# 1 ltor = 6 metros
+#lata de 18 litros = 80
+#galoes de 3,6 = 35
+
+area = input("Qual a área a ser pintada (m²): ")
